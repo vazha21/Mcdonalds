00f418833adc15a548975cdf11f0da809bf6aa49
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2020-10-16 16:22:15 +0400

    made slideshow responsive

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 var slider_img = document.querySelector('.slider-img');
-var images = ['slide-1.jpg','slide-2.jpg','slide-3.jpg','slide-4.jpg','slide-5.jpg',]
+var current_image_index = 0;
 var slideShowImageDiv = document.querySelector('.slide-show-image');
 var circleLinks = document.querySelectorAll('.circle');
 
-var current_image_index = 0;
+
+
+// changes background images when screen becomes 700 px or smaller.
+function responsivness(x){
+    if (x.matches)
+    {
+        images = ['slide-resp-1.jpg','slide-resp-3.jpg','slide-resp-4.jpg','slide-resp-5.jpg'];
+        image_path = 'url(./images/slide-show/' + images[current_image_index] + ')';
+        document.querySelector('.slide-show-image').style.backgroundImage = image_path ;
+        document.getElementById('previousButton').style.fontSize = '40px';
+        document.getElementById('nextButtton').style.fontSize = '40px';
+    }
+    else
+    {
+        images =  ['slide-1.jpg','slide-3.jpg','slide-4.jpg','slide-5.jpg'];
+        image_path = 'url(./images/slide-show/' + images[current_image_index] + ')';
+        document.querySelector('.slide-show-image').style.backgroundImage = image_path ;
+    }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+responsivness(x);
+x.addListener(responsivness);
+
+
 
 
 // changes picture when clicked on cricles. also changes current image index.
@@ -46,7 +70,7 @@ function setImg(path,current_image_index){
 
 
 function paginate(current_image_index){
-    for (let index = 0; index < circleLinks.length; index++) {
+    for (let index = 0; index < circleLinks.length; index++) {  
         if (index === current_image_index){
             circleLinks[index].style.background = 'white';
         }
